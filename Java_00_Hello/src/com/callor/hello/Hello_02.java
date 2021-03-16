c8efa7180ae2289e8563a3c0b6a9cb879d383557
wjdfbdns819 <wjdfbdns819@wjdfbdns819.com>
2021-03-16 15:43:30 +0900

    Hello

--- Java_00_Hello/src/com/callor/hello/Hello_02.java
+++ Java_00_Hello/src/com/callor/hello/Hello_02.java
@@ -4,6 +4,6 @@ public class Hello_02 {
 
 	public static void main(String[] ar) {
 		System.out.print("반갑습니다");
-		
+		System.out.print(30 * 40);
 	}
 }
---
+++ Java_00_Hello/src/com/callor/hello/Hello_03.java
@@ -0,0 +1,14 @@
+package com.callor.hello;
+
+public class Hello_03 {
+
+	public static void main(String[] args) {
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 % 40);
+ 
+	}
+
+}
