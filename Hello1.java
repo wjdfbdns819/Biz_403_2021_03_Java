ab4eb2228d31a32e525722a3c4977810b89fe171
wjdfbdns819 <wjdfbdns819@wjdfbdns819.com>
2021-03-16 14:21:41 +0900

    처음

---
+++ Hello1.java
@@ -0,0 +1,11 @@
+public class Hello1 {
+
+	public static void main(String[] arg) {
+
+		System.out.println(30 * 50);
+
+	}
+
+
+
+}
---
+++ Java_00_Hello/src/com/callor/hello/Nums.java
@@ -0,0 +1,12 @@
+package com.callor.hello;
+
+public class Nums {
+
+	public static void main(String[] args) {
+		System.out.println(3);
+		System.out.println("과");
+		System.out.println(5);
+		System.out.println("을 더하면");
+		System.out.println("과");
+	}
+}
