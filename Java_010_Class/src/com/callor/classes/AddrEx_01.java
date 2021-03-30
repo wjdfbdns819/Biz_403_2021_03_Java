f02487363b0f675c01f731af7d5e10aaa4073331
wjdfbdns819 <wjdfbdns819@wjdfbdns819.com>
2021-03-30 16:38:57 +0900

    수업_03_30

---
+++ Java_010_Class/src/com/callor/classes/AddrEx_01.java
@@ -0,0 +1,19 @@
+package com.callor.classes;
+
+import com.callor.classes.model.AddressVO;
+
+public class AddrEx_01 {
+
+	public static void main(String[] args) {
+		
+		AddressVO aVO = new AddressVO();
+		aVO.setName("홍길동");
+		aVO.setAddr("서울식");
+		aVO.setTel("010-111-1111");
+	
+		
+		
+		
+		
+	}
+}
