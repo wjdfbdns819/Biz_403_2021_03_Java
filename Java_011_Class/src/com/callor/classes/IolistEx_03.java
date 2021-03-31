fe5c9b2295dae673225d2d8ce9c1780403196e54
wjdfbdns819 <wjdfbdns819@wjdfbdns819.com>
2021-03-31 16:35:26 +0900

    수업_03_31

---
+++ Java_011_Class/src/com/callor/classes/IolistEx_03.java
@@ -0,0 +1,14 @@
+package com.callor.classes;
+
+import com.callor.classes.service.IolistServiceV1;
+
+public class IolistEx_03 {
+
+	public static void main(String[] args) {
+		
+		IolistServiceV1 ioVO = new IolistServiceV1();
+		
+		ioVO.selectMenu();
+		
+	}
+}
